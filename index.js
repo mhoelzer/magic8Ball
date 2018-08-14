a286e89374848d7da04b8dabec6e74c8699ddfd6
Michelle Hoelzer <hoelzermm@gmail.com>
2018-08-14 15:45:30 -0400

    have inital stuff done; do styling later

---
+++ index.js
@@ -0,0 +1,10 @@
+document.getElementById("ballButt").onclick = function() {
+    let answers = ["'Tis certain", "Beyond a doubteth", "My reply be no", "Answ'r is unc'rtain. Tryeth again lat'r", "V'ry doubtful", "Most unlikely", "As I seeth 't, Aye", "It be decidedly so", "Thee may relyeth on it", "Signs pointeth to Aye", "Better not be tellin' ye now", "Cannot predicteth anon", "Concentrateth an' asketh again", "Counteth not on 't", "Mine own sources sayeth nay", "Outlook nay so valorous"];
+    let ballGivesAnswers = answers[Math.floor(Math.random() * answers.length)]
+    document.getElementById('ballSpeaketh').innerHTML = ballGivesAnswers;
+    /* the stuff below doesn't make it generate randomly and doesn't clear previous response onclick
+    // let typedText = document.getElementById("questionInput");
+    // typedText = document.createTextNode(ballGivesAnswers);
+    // document.getElementById("ballSpeaketh").appendChild(typedText);
+    */
+}
