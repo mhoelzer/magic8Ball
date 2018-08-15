f88f955a77d55d75db4d1f05ec48124183316d2f
Michelle Hoelzer <hoelzermm@gmail.com>
2018-08-15 15:35:14 -0400

    spinning

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+function replace() { 
+    document.getElementById("startBall").style.display="none"; 
+    document.getElementById("mainStuff").style.display="block"; 
+}
+function replaceBack() { 
+    document.getElementById("startBall").style.display="block"; 
+    document.getElementById("mainStuff").style.display="none"; 
+}
+
 document.getElementById("ballButt").onclick = function() {
     let answers = ["'Tis certain", "Beyond a doubteth", "My reply be no", "Answ'r is unc'rtain. Tryeth again lat'r", "V'ry doubtful", "Most unlikely", "As I seeth 't, Aye", "It be decidedly so", "Thee may relyeth on it", "Signs pointeth to Aye", "Better not be tellin' ye now", "Cannot predicteth anon", "Concentrateth an' asketh again", "Counteth not on 't", "Mine own sources sayeth nay", "Outlook nay so valorous"];
     let ballGivesAnswers = answers[Math.floor(Math.random() * answers.length)]
