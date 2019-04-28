ea2758c251f8ddaeb36c4316aca0fce8f8195870
Michelle Hoelzer <hoelzermm@gmail.com>
2019-04-28 16:16:49 -0400

    added empty string

--- index.js
+++ index.js
@@ -8,12 +8,18 @@ function replaceBack() {
 }
 
 document.getElementById("ballButt").onclick = function() {
-    let answers = ["'Tis certain", "Beyond a doubteth", "My reply be no", "Answ'r is unc'rtain. Tryeth again lat'r", "V'ry doubtful", "Most unlikely", "As I seeth 't, Aye", "It be decidedly so", "Thee may relyeth on it", "Signs pointeth to Aye", "Better not be tellin' ye now", "Cannot predicteth anon", "Concentrateth an' asketh again", "Counteth not on 't", "Mine own sources sayeth nay", "Outlook nay so valorous"];
-    let ballGivesAnswers = answers[Math.floor(Math.random() * answers.length)];
-    document.getElementById('ballSpeaketh').innerHTML = ballGivesAnswers;
-    /* the stuff below doesn't make it generate randomly and doesn't clear previous response onclick
-    // let typedText = document.getElementById("questionInput");
-    // typedText = document.createTextNode(ballGivesAnswers);
-    // document.getElementById("ballSpeaketh").appendChild(typedText);
-    */
+    if (document.getElementById("questionInput").value == "") {
+        let enterThing = "Ent'r a questioneth to taketh an answer!"
+        document.getElementById('ballSpeaketh').innerHTML = enterThing;
+    }
+    else {
+        let answers = ["'Tis certain", "Beyond a doubteth", "My reply be no", "Answ'r is unc'rtain. Tryeth again lat'r", "V'ry doubtful", "Most unlikely", "As I seeth 't, Aye", "It be decidedly so", "Thee may relyeth on it", "Signs pointeth to Aye", "Better not be tellin' ye now", "Cannot predicteth anon", "Concentrateth an' asketh again", "Counteth not on 't", "Mine own sources sayeth nay", "Outlook nay so valorous"];
+        let ballGivesAnswers = answers[Math.floor(Math.random() * answers.length)];
+        document.getElementById('ballSpeaketh').innerHTML = ballGivesAnswers;
+        /* the stuff below doesn't make it generate randomly and doesn't clear previous response onclick
+        // let typedText = document.getElementById("questionInput");
+        // typedText = document.createTextNode(ballGivesAnswers);
+        // document.getElementById("ballSpeaketh").appendChild(typedText);
+        */
+    }
 }
